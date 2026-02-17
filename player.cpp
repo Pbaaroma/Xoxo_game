c1bea706b0e1cfa5a0edcc072708749b091330da
Gabriel Boafo <boafokofigabriel@gmail.com>
2026-02-17 00:43:42 -0600

    Add the player files

--- player.cpp
+++ player.cpp
@@ -1,6 +1,36 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Hello World!"<<endl;
-    return 0;
+/**
+ * @file player.cpp
+ * @author Gabriel Kofi Coffie Boafo & Emmanuel Mensah
+ * @date  02-16-2026
+ * @brief This file contains the implementation of the Player class which represents a player in the game.
+ */
+
+
+#include <iostream>
+#include "player.h"
+
+ // Default construor
+Player::Player() : symbol('X'), name("player") {}
+
+
+// setter for the player's name
+void Player::setName(const std::string& playerName) {
+    name = playerName;
 }
+
+// setter for the player's symbol
+void Player::setSymbol(char playerSymbol) {
+    symbol = playerSymbol;
+}
+
+// getter for the player's name
+std::string Player::getName() const {
+    return name;
+}
+
+// getter for the player's symbol
+char Player::getSymbol() const {
+    return symbol;
+}
+
+
--- player.h
+++ player.h
@@ -1,13 +1,36 @@
+/**
+ * @file player.h
+ * @author Gabriel Kofi Coffie Boafo & Emmanuel Mensah
+ * @brief This file contains the definition of the Player class which represents a player in the game.
+ */
+
 #ifndef PLAYER_H
 #define PLAYER_H
-
+#include <string>
 
 class Player {
+    private:
+        std::string name; // Player's name
+    char symbol; // X or O 
+
+
 public:
-    Player(char symbol) : symbol(symbol) {}
+
+// constructor for the Player class. It takes a symbol and a name as parameters and initializes the player's symbol and name.
+Player() = default; // Default constructor
+    Player(char symbol, const std::string& name) : symbol(symbol), name(name) {}
     char getSymbol() const { return symbol; }
-private:
-    char symbol;
+    const std::string& getName() const { return name; }
+
+    // Setter for the player's name
+    void setName(const std::string& playerName);
+    void setSymbol(char playerSymbol);
+
+    // Getters for the player's name and symbol
+    std:: string getName() const;
+    char getSymbol() const;
+
+    
 };      
 
 #endif // PLAYER_H
